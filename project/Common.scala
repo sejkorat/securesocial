a5ed20ea6c59639fd4b006f3971a8690099226f0
sejkorat <sejkorat@post.cz>
2015-11-03 14:34:04 +0100

    kompilace pro scala 2.10.4

--- project/Common.scala
+++ project/Common.scala
@@ -3,5 +3,5 @@
 object Common {
   def version = "master-SNAPSHOT"  
   def playVersion = System.getProperty("play.version", "2.3.7")
-  def scalaVersion =  System.getProperty("scala.version", "2.11.1")
+  def scalaVersion =  System.getProperty("scala.version", "2.10.4")
 }
