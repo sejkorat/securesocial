5f6fe64ae332debac1dc842f8c38c25f422dee2f
sejkorat <sejkorat@post.cz>
2015-11-03 14:53:25 +0100

    zmena verze projektu

--- project/Common.scala
+++ project/Common.scala
@@ -1,7 +1,7 @@
 
 
 object Common {
-  def version = "master-SNAPSHOT"  
+  def version = "master-play-2.3"  
   def playVersion = System.getProperty("play.version", "2.3.7")
   def scalaVersion =  System.getProperty("scala.version", "2.10.4")
 }
